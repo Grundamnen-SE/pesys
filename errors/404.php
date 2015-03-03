9147bdaaa482186da41f65e7a57fa12bfa897a6a
Gustav Ekner <gustav.ekner@gmail.com>
2015-03-03 20:30:01 +0100

    404.php

--- errors/404.php
+++ errors/404.php
@@ -5,7 +5,7 @@
     <meta name="author" content="Joar Classon, Gustav Ekner och Alicia Palmér">
     <meta name="description" content="Periodiska Systemet, Enkelt och informativt (*CHANGE*)">
     <title>404 - Sidan hittades inte! - Grundämnen.se</title>
-    <link rel="stylesheet/less" type="text/css" href="/css/errors.less" />
+    <link rel="stylesheet/less" type="text/css" href="../css/errors.less" />
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.4/less.min.js"></script>
     <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
   </head>
