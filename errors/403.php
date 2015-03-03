6d1cda66fcd1817c82c0e1d401a8010f20fa8332
Joar Classon <joar@classon.se>
2015-03-03 20:50:49 +0100

    Fixade meta tagen

--- errors/403.php
+++ errors/403.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="author" content="Joar Classon, Gustav Ekner och Alicia Palmér">
-    <meta name="description" content="Periodiska Systemet, Enkelt och informativt (*CHANGE*)">
+    <meta name="description" content="Här kan du se och läsa om alla grundämnen på ett nytt och spännande sätt.">
     <title>403 - Permission Denied - Grundämnen.se</title>
     <link rel="stylesheet/less" type="text/css" href="../css/errors.less" />
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.4/less.min.js"></script>
--- errors/404.php
+++ errors/404.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="author" content="Joar Classon, Gustav Ekner och Alicia Palmér">
-    <meta name="description" content="Periodiska Systemet, Enkelt och informativt (*CHANGE*)">
+    <meta name="description" content="Här kan du se och läsa om alla grundämnen på ett nytt och spännande sätt.">
     <title>404 - Sidan hittades inte - Grundämnen.se</title>
     <link rel="stylesheet/less" type="text/css" href="../css/errors.less" />
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.4/less.min.js"></script>
