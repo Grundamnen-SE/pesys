1935d6cb4c2fc7468d61921b768c16eae08d2169
Joar Classon <joar@classon.se>
2015-03-03 20:32:09 +0100

    Added 403 and 500.

---
+++ errors/403.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Joar Classon, Gustav Ekner och Alicia Palmér">
+    <meta name="description" content="Periodiska Systemet, Enkelt och informativt (*CHANGE*)">
+    <title>404 - Sidan hittades inte! - Grundämnen.se</title>
+    <link rel="stylesheet/less" type="text/css" href="/css/errors.less" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.4/less.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div>
+      <h1>
+        403 - Permission Denied!
+      </h1>
+      <span>Grundämnen.se<?= $_SERVER['REQUEST_URI'] ?></span>
+      <p>Du får inte gå in på den sidan/mapp! Tryck här för att gå till <a href="/">startsidan!</a></p>
+    </div>
+  </body>
+</html>
