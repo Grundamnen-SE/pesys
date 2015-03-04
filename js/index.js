b20516d4e9ba74f93b5840e47647b11174b7b748
Joar Classon <joar@classon.se>
2015-03-05 00:13:16 +0100

    minska rut storlek och minska padding på td-logo

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ $("#advmode").on('change', function() {
 // Kanske har någon betydelse...
 $( document ).ready(function() {
   var width = $('td').width();
-  var height = width - 5;
+  var height = width - 20;
   $("td").css("height", height);
   $(".td-logo").css("height", "50px");
 });
