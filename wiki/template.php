6133543c6a9c8fe7b569cc06ffcf5952e7fc6e2e
Joar Classon <joar@classon.se>
2015-03-04 21:30:45 +0100

    fixing some visual stuff with the popup and exit button

--- wiki/template.php
+++ wiki/template.php
@@ -4,12 +4,11 @@
   $file = "pages/" . $_POST['file'] . ".html";
   if (file_exists($file)) {
 ?>
-
+<div class="info">
   <?php include ($file); ?>
-
+</div>
 <?php
   } else {
     echo "<h1>Detta ämne har vi inte skrivit om än!</h1>";
-    echo $file;
   }
 ?>
