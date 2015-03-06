c3ff5a0a49a47a3dd6940c802b1c64e9993471a7
Joar Classon <joar@classon.se>
2015-03-06 17:05:20 +0100

    ändra email address

--- wiki/template.php
+++ wiki/template.php
@@ -7,11 +7,10 @@
 <div class="info">
   <?php include ($file); ?>
 </div>
-<h3>Är något fel? Är det något du vill lägga till? Maila oss så lägger vi till din ändring!</h3>
+<h3>Är något fel? Är det något du vill lägga till? <a href="mailto:info.grundamnen@gmail.com">Maila oss</a> så lägger vi till din ändring!</h3>
 <?php
   } else {
     echo "<h1>Detta ämne har vi inte skrivit om än!</h1>";
-    echo "<h2>Vill du hjälpa till att skriva en sida? Skicka in din text till oss via mail så lägger vi till den till ämnet. </h2>";
-    echo '<img src="/img/grundamnen-kontakt.png">';
+    echo "<h2>Vill du hjälpa till att skriva en sida? Skicka in din text till oss via <a href="mailto:info.grundamnen@gmail.com">mail</a> så lägger vi till den till ämnet. </h2>";
   }
 ?>
