0e32af73569ce64de63e621667f8133bbd51c9b0
Joar Classon <joar@classon.se>
2015-03-06 17:03:00 +0100

    Vill du hjälpa till att fylla på med information på wikin så kolla här :)

--- wiki/template.php
+++ wiki/template.php
@@ -7,8 +7,11 @@
 <div class="info">
   <?php include ($file); ?>
 </div>
+<h3>Är något fel? Är det något du vill lägga till? Maila oss så lägger vi till din ändring!</h3>
 <?php
   } else {
     echo "<h1>Detta ämne har vi inte skrivit om än!</h1>";
+    echo "<h2>Vill du hjälpa till att skriva en sida? Skicka in din text till oss via mail så lägger vi till den till ämnet. </h2>";
+    echo '<img src="/img/grundamnen-kontakt.png">';
   }
 ?>
