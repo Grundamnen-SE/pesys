878a954910b3a1106861e19ecf7c6fb97ac55cf9
Joar Classon <joar@classon.se>
2015-03-07 20:02:30 +0100

    Faktiskt funkar nu?

--- includes/url_handler.php
+++ includes/url_handler.php
@@ -3,14 +3,14 @@ $dir = "wiki/pages/";
 $link = $_GET['simple_link'];
 $link_r = explode("/", $link);
 print_r($link_r);
-if (file_exists($dir.$link_r[1].".html")) {
+if (file_exists($dir.$link_r[0].".html")) {
 ?>
 <script>
 $("body").css({"overflow":"hidden"});
 $.ajax({
   type: "POST",
   url: "wiki/template.php",
-  data: {"file": "<?= $link_r[1] ?>"},
+  data: {"file": "<?= $link_r[0] ?>"},
   success: function( data ) {
     $("#newHTML").append(data);
     $("#newHTML").show("scale", 300, function () {});
