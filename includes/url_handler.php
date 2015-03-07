359d9eabc0b79dbffc588c4e8f8a5a08a240b295
Joar Classon <joar@classon.se>
2015-03-07 19:57:00 +0100

    Second version

--- includes/url_handler.php
+++ includes/url_handler.php
@@ -1,19 +1,27 @@
 <?php
 $dir = "wiki/pages/";
-if (file_exists($dir.$_GET['simple_link'].".html")) {
+$link = $_GET['simple_link'];
+$link_r = explode("/", $link);
+if (file_exists($dir.$link_r[1].".html")) {
 ?>
 <script>
+$("body").css({"overflow":"hidden"});
 $.ajax({
   type: "POST",
   url: "wiki/template.php",
-  data: {"file": "<?= $_GET['simple_link'] ?>"},
+  data: {"file": "<?= $link_r[1] ?>"},
   success: function( data ) {
     $("#newHTML").append(data);
-    $("body").css({"overflow":"hidden"});
     $("#newHTML").show("scale", 300, function () {});
   },
   dataType: "html"
 });
+</script>
+<?php
+} else {
+?>
+<script>
+$( document ).html(<?php include("errors/404.php"); ?>);
 </script>
 <?php
 }
