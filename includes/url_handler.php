b857076a49dc6d236119b22bece3932ce496e82c
Joar Classon <joar@classon.se>
2015-03-07 20:30:30 +0100

    Added close button to 404 with url-handler

--- includes/url_handler.php
+++ includes/url_handler.php
@@ -23,7 +23,7 @@ $.ajax({
 ?>
 <script>
 $("body").css({"overflow":"hidden"});
-$("#newHTML").append("<h1>Detta ämne har vi inte skrivit om än! (Eller så finns inte ämnet i det kända periodiska system!)</h1><h2>Vill du hjälpa till att skriva en sida? Skicka in din text till oss via <a href=\"mailto:info.grundamnen@gmail.com\">mail</a> så lägger vi till den till ämnet. </h2>");
+$("#newHTML").append('<div onclick="exit();" id="exit">X</div><h1>Detta ämne har vi inte skrivit om än! (Eller så finns inte ämnet i det kända periodiska system!)</h1><h2>Vill du hjälpa till att skriva en sida? Skicka in din text till oss via <a href="mailto:info.grundamnen@gmail.com">mail</a> så lägger vi till den till ämnet. </h2>');
 $("#newHTML").show("scale", 300, function () {});
 </script>
 <?php
