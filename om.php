9b0bf215870fcc3efc0e6165ab9ced76b7344474
Joar Classon <joar@classon.se>
2015-03-08 11:07:22 +0100

    Fixat lite mer format och skrivit på vad jag har lärt mig

--- om.php
+++ om.php
@@ -49,12 +49,12 @@
       <div class="why">
         <h1>Varför ska vi vinna tävlingen?</h1>
         <p>
-De periodiska system och allmänt den fakta vi har att tillgå i skolan om grundämnen och atomer kommer ifrån de fysik och kemiböcker vi läser i under lektionerna samt de planscher som sitter uppsatta i klassrummen och i korridorerna. Forskning kring atomfysik, nya material och kemiska reaktioner pågår dagligen runt om i världen och vi får hela tiden ny användbar information. Böckerna vi har i skolan kan man så klart inte trycka om varje läsår både på grund av ekonomiska skäl men också med tanke på miljön. Detta gör dock att det som står i dem inte är den senaste informationen som finns att tillgå. Allt behöver till och med inte vara korrekt.
-Internet däremot är en utav de främsta medierna för att distribuera och få ny information på. Därför tycker vi att det är viktigt att internet kan användas som en källa för naturvetenskaplig information och här kommer grundämnen.se perfekt till hands, både för lärare och elever!
-Vi har tänkt att denna hemsida kan användas som ett hjälpmedel i undervisningen på det sätt att läraren kan hålla i genomgångar om periodiska systemet och atomer på t.ex en projektor eller en smartboard. Att få se bilder och videor istället för att bara läsa text är ett bra sätt att lära in information och fakta på. Att dessutom relatera de kunskaper som man lär sig på lektionen till miljön och samhället vi lever i ökar kunskapen och förståelsen i nutiden, men också för framtida kunskaper i fysik, kemi och biologi. Ju mer man förstår i grunden, desto lättare blir det att förstå andra saker som är byggda på den. I det här fallet t.ex joner, fotosyntesen eller kärnfysik.
-Denna hemsida är även ett bra sätt för oss "skapare" att relatera de kunskaper vi lär oss under fysiken och kemin och kunna förmedla den till andra, kanske framförallt i vår ålder. På så sätt måste vi kommunicera med besökaren på ett sätt som är intresseväckande samtidigt som det är lärorikt. Vi måste också använda vetenskapliga begrepp samt förklara dessa på ett sätt som man förstår samt anpassa texten till mottagaren.
-Vi har även försökt att anpassa fakta på sidan så att det kan passa mellanstadieelever som ser ett periodiskt system för första gången samt för den lite mer erfarne eleven som kommit längre i studierna.
-Vi har försökt använda flera medier: video, text och bilder, för att lära ut andra. Alla lär sig inte bäst av bara text, utan vissa kan behöva bilder och filmer. Vi har försökt anpassa oss så gott det går på det sättet, så att så många som möjligt kan få ha användning av vår hemsida.
+          De periodiska system och allmänt den fakta vi har att tillgå i skolan om grundämnen och atomer kommer ifrån de fysik och kemiböcker vi läser i under lektionerna samt de planscher som sitter uppsatta i klassrummen och i korridorerna. Forskning kring atomfysik, nya material och kemiska reaktioner pågår dagligen runt om i världen och vi får hela tiden ny användbar information. Böckerna vi har i skolan kan man så klart inte trycka om varje läsår både på grund av ekonomiska skäl men också med tanke på miljön. Detta gör dock att det som står i dem inte är den senaste informationen som finns att tillgå. Allt behöver till och med inte vara korrekt.
+          Internet däremot är en utav de främsta medierna för att distribuera och få ny information på. Därför tycker vi att det är viktigt att internet kan användas som en källa för naturvetenskaplig information och här kommer grundämnen.se perfekt till hands, både för lärare och elever!
+          Vi har tänkt att denna hemsida kan användas som ett hjälpmedel i undervisningen på det sätt att läraren kan hålla i genomgångar om periodiska systemet och atomer på t.ex en projektor eller en smartboard. Att få se bilder och videor istället för att bara läsa text är ett bra sätt att lära in information och fakta på. Att dessutom relatera de kunskaper som man lär sig på lektionen till miljön och samhället vi lever i ökar kunskapen och förståelsen i nutiden, men också för framtida kunskaper i fysik, kemi och biologi. Ju mer man förstår i grunden, desto lättare blir det att förstå andra saker som är byggda på den. I det här fallet t.ex joner, fotosyntesen eller kärnfysik.
+          Denna hemsida är även ett bra sätt för oss "skapare" att relatera de kunskaper vi lär oss under fysiken och kemin och kunna förmedla den till andra, kanske framförallt i vår ålder. På så sätt måste vi kommunicera med besökaren på ett sätt som är intresseväckande samtidigt som det är lärorikt. Vi måste också använda vetenskapliga begrepp samt förklara dessa på ett sätt som man förstår samt anpassa texten till mottagaren.
+          Vi har även försökt att anpassa fakta på sidan så att det kan passa mellanstadieelever som ser ett periodiskt system för första gången samt för den lite mer erfarne eleven som kommit längre i studierna.
+          Vi har försökt använda flera medier: video, text och bilder, för att lära ut andra. Alla lär sig inte bäst av bara text, utan vissa kan behöva bilder och filmer. Vi har försökt anpassa oss så gott det går på det sättet, så att så många som möjligt kan få ha användning av vår hemsida.
         </p>
       </div>
       <div class="line">
@@ -62,15 +62,15 @@ Vi har försökt använda flera medier: video, text och bilder, för att lära u
       <div class="syfte"></div>
         <h1>Syfte och mottagare:</h1>
         <p>
-Periodiska systemet och framför allt grundämnena är grunden till mycket av det vi läser i de naturorienterade ämnena i skolan, men också för hela vår värld. Därför är det något som är extra viktigt att förstå samt att det underlättar väldigt mycket när man väl förstår det. Att läsa en massa sidor i en bok med bokstäver och siffror hit och dit är inte ett effektivt sätt för alla att lära in information på. Därför blir det väldigt lätt att man börjar syssla med annat under lektionstid och ingenting blir gjort. Det kanske inte visar sig för stunden men i vidare studier blir det ett klart hinder.
-Syftet med denna hemsida är därför att skapa ett studiehjälpmedel som är uppdaterat, färgglatt och anpassat till dagens tekniska samhälle som kan hjälpa både elever och lärare i undervisningen.
-Hemsidan är såklart välkommen att besökas av alla, men den målgrupp vi främst har riktat hemsidan till är elever och lärare.
+          Periodiska systemet och framför allt grundämnena är grunden till mycket av det vi läser i de naturorienterade ämnena i skolan, men också för hela vår värld. Därför är det något som är extra viktigt att förstå samt att det underlättar väldigt mycket när man väl förstår det. Att läsa en massa sidor i en bok med bokstäver och siffror hit och dit är inte ett effektivt sätt för alla att lära in information på. Därför blir det väldigt lätt att man börjar syssla med annat under lektionstid och ingenting blir gjort. Det kanske inte visar sig för stunden men i vidare studier blir det ett klart hinder.
+          Syftet med denna hemsida är därför att skapa ett studiehjälpmedel som är uppdaterat, färgglatt och anpassat till dagens tekniska samhälle som kan hjälpa både elever och lärare i undervisningen.
+          Hemsidan är såklart välkommen att besökas av alla, men den målgrupp vi främst har riktat hemsidan till är elever och lärare.
         </p>
       <div class="line"></div>
       <div class="webben">
         <h1>Vad tillför webben i ert skolarbete? </h1>
         <p>
-Denna hemsida är även ett bra sätt för oss "skapare" att relatera de kunskaper vi lär oss under fysiken och kemin och kunna lära ut den till andra i vår ålder. På så sätt måste vi kommunicera med besökaren på ett sätt som intresseväckande samtidigt som det är lärorikt. Vi måste också använda vetenskapliga begrepp samt förklara dessa på ett sätt som man förstår samt anpassa texten till mottagaren samt själva granska vad vi skriver så att det är förståeligt.
+          Denna hemsida är även ett bra sätt för oss "skapare" att relatera de kunskaper vi lär oss under fysiken och kemin och kunna lära ut den till andra i vår ålder. På så sätt måste vi kommunicera med besökaren på ett sätt som intresseväckande samtidigt som det är lärorikt. Vi måste också använda vetenskapliga begrepp samt förklara dessa på ett sätt som man förstår samt anpassa texten till mottagaren samt själva granska vad vi skriver så att det är förståeligt.
         </p>
       </div>
       <div class="line"></div>
@@ -83,43 +83,37 @@ Denna hemsida är även ett bra sätt för oss "skapare" att relatera de kunskap
           När vi har jobbat i skolan har det antingen varit under Datorklubben, varje onsdag där andra i skolan kan komma och spela eller programmera tillsammans, eller under lov, då vi kommmer och filmer lite med greenscreen tack vare vår Sysadmin på skolan.
           Vi har också åkt iväg till Ytterby gruva under en dag och utforskat den gamla gruvan och området där 9 ämnen har upptäcktes, vilket är flest i hela världen samt att 4 av ämnena är döpta efter platsen. Vi filma videoklipp om alla ämnen som upptäcktes där och det var en intressant dag där.
         </p>
+        <h1>Vad har vi lärt oss?</h1>
+        <h2>Joar</h2>
+        <p>
+          Jag har lärt mig ganska mycket faktiskt. Jag har lite problem att lära mig saker utan lär mig bäst när jag är intresserad i det jag lär mig om. Grundämnena kan vara svåra att förstå men under detta projektsgång så har jag lärt mig mer än jag har gjort under skolgången då vi jobbade med detta. Jag har även lärt mig mer om Ytterby, att det stället faktiskt finns, och att det finns fler ämnen runt om dig i vardagen än vad man faktiskt kan tro.
+        </p>
       </div>
       <div class="line"></div>
       <div class="">
         <h1>Hur kopplas bidraget till läroplanen?</h1>
         <h2>Syftet i ämnet Svenska:</h2>
         <p>
-Undervisningen i ämnet svenska ska syfta till att eleverna utvecklar kunskaper i och om svenska språket. Genom undervisningen ska eleverna ges möjlighet att utveckla sin förmåga att kommunicera och anpassa sitt språk till olika sammanhang och för olika syften. Undervisningen ska även bidra till att eleverna utvecklar tal, skrift och andra kommunikationsformer. Vidare ska undervisningen bidra till att eleverna utvecklar sin läsförmåga och sitt intresse för att läsa och skriva.
-
-<ul>
-  <li>Kommunikation för olika syften</li>
-
-  <li>Muntliga presentationer. Stödord, bilder och digitala medier som verktyg för att planera och genomföra en presentation.</li>
-
-  <li>Handstil och att skriva på dator.</li>
-
-  <li>Informationssökning i olika medier och källor, till exempel i uppslagsböcker, genom intervjuer och via sökmotorer på internet.</li>
-</ul>
+          Undervisningen i ämnet svenska ska syfta till att eleverna utvecklar kunskaper i och om svenska språket. Genom undervisningen ska eleverna ges möjlighet att utveckla sin förmåga att kommunicera och anpassa sitt språk till olika sammanhang och för olika syften. Undervisningen ska även bidra till att eleverna utvecklar tal, skrift och andra kommunikationsformer. Vidare ska undervisningen bidra till att eleverna utvecklar sin läsförmåga och sitt intresse för att läsa och skriva.
+          <ul>
+            <li>Kommunikation för olika syften</li>
+            <li>Muntliga presentationer. Stödord, bilder och digitala medier som verktyg för att planera och genomföra en presentation.</li>
+            <li>Handstil och att skriva på dator.</li>
+            <li>Informationssökning i olika medier och källor, till exempel i uppslagsböcker, genom intervjuer och via sökmotorer på internet.</li>
+          </ul>
         </p>
         <h2>Syftet i ämnet Fysik:</h2>
         <p>
-Undervisningen i ämnet fysik ska syfta till att eleverna utvecklar kunskaper om fysikaliska sammanhang och nyfikenhet på och intresse för att undersöka omvärlden. Genom undervisningen ska eleverna ges möjlighet att ställa frågor om fysikaliska företeelser och sammanhang utifrån egna upplevelser och aktuella händelser. Vidare ska undervisningen ge eleverna förutsättningar att söka svar på frågor med hjälp av både systematiska undersökningar och olika typer av källor. På så sätt ska undervisningen bidra till att eleverna utvecklar ett kritiskt tänkande kring sina egna resultat, andras argument och olika informationskällor. Genom undervisningen ska eleverna också utveckla förståelse för att påståenden kan prövas och värderas med hjälp av naturvetenskapliga arbetsmetoder.
-
-<ul>
-  <li>Aktuella samhällsfrågor som rör fysik.</li>
-
-  <li>Historiska och nutida upptäckter inom fysikområdet och hur de har formats av och format världsbilder. Upptäckternas betydelse för teknik, miljö, samhälle och människors levnadsvillkor.</li>
-
-  <li>Universums utveckling och atomslagens uppkomst genom stjärnornas utveckling.</li>
-
-  <li>De fysikaliska modellernas och teoriernas användbarhet, begränsningar, giltighet och föränderlighet.</li>
-
-  <li>Sambandet mellan fysikaliska undersökningar och utvecklingen av begrepp, modeller och teorier.</li>
-
-  <li>Dokumentation av undersökningar med tabeller, diagram, bilder och skriftliga rapporter.</li>
-
-  <li>Källkritisk granskning av information och argument som eleven möter i källor och samhällsdiskussioner med koppling till fysik.</li>
-</ul>
+          Und ervisningen i ämnet fysik ska syfta till att eleverna utvecklar kunskaper om fysikaliska sammanhang och nyfikenhet på och intresse för att undersöka omvärlden. Genom undervisningen ska eleverna ges möjlighet att ställa frågor om fysikaliska företeelser och sammanhang utifrån egna upplevelser och aktuella händelser. Vidare ska undervisningen ge eleverna förutsättningar att söka svar på frågor med hjälp av både systematiska undersökningar och olika typer av källor. På så sätt ska undervisningen bidra till att eleverna utvecklar ett kritiskt tänkande kring sina egna resultat, andras argument och olika informationskällor. Genom undervisningen ska eleverna också utveckla förståelse för att påståenden kan prövas och värderas med hjälp av naturvetenskapliga arbetsmetoder.
+          <ul>
+            <li>Aktuella samhällsfrågor som rör fysik.</li>
+            <li>Historiska och nutida upptäckter inom fysikområdet och hur de har formats av och format världsbilder. Upptäckternas betydelse för teknik, miljö, samhälle och människors levnadsvillkor.</li>
+            <li>Universums utveckling och atomslagens uppkomst genom stjärnornas utveckling.</li>
+            <li>De fysikaliska modellernas och teoriernas användbarhet, begränsningar, giltighet och föränderlighet.</li>
+            <li>Sambandet mellan fysikaliska undersökningar och utvecklingen av begrepp, modeller och teorier.</li>
+            <li>Dokumentation av undersökningar med tabeller, diagram, bilder och skriftliga rapporter.</li>
+            <li>Källkritisk granskning av information och argument som eleven möter i källor och samhällsdiskussioner med koppling till fysik.</li>
+          </ul>
         </p>
       </div>
       <div class="line"></div>
