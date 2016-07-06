c165f547ef6c8a20c671d71c780867fc9c901786
Gustav Ekner <gustav.ekner@gmail.com>
2016-07-06 23:37:39 +0200

    Första versionen av nodejs-versionen av grundämnen. Startsidan är den enda sidan som funkar, än så länge

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(8080);
